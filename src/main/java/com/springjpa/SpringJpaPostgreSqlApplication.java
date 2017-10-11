5b37027dab8b6a4b247740b89422339cf7359b6a
Hendi Santika <hendisantika@gmail.com>
2017-10-12 05:38:06 +0700

    #2 - Added Main App

---
+++ src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -0,0 +1,21 @@
+package com.springjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springJPA-postgreSQL
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/10/17
+ * Time: 05.36
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootApplication
+public class SpringJpaPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaPostgreSqlApplication.class, args);
+	}
+}
