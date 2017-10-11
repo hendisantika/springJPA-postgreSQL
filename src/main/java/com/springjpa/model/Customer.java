eb377cc0f71e3730bba12c1bac46d0378b529065
Hendi Santika <hendisantika@gmail.com>
2017-10-12 05:38:32 +0700

    #3 - Added Customer Model

---
+++ src/main/java/com/springjpa/model/Customer.java
@@ -0,0 +1,42 @@
+package com.springjpa.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springJPA-postgreSQL
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/10/17
+ * Time: 05.39
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+	private static final long serialVersionUID = -3009157732242241606L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+
+	@Column(name = "firstname")
+	private String firstName;
+
+	@Column(name = "lastname")
+	private String lastName;
+
+	protected Customer() {
+	}
+
+	public Customer(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
+	}
+}
