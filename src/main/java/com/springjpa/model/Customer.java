891b0bb99131ef487ce9f53a2d9feca795f80833
Hendi Santika <hendisantika@gmail.com>
2020-02-24 20:26:42 +0700

    #25 - Add lombok lib

--- src/main/java/com/springjpa/model/Customer.java
+++ src/main/java/com/springjpa/model/Customer.java
@@ -1,6 +1,15 @@
 package com.springjpa.model;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 /**
  * Created by IntelliJ IDEA.
@@ -14,6 +23,9 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "customer")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer implements Serializable {
 
 	private static final long serialVersionUID = -3009157732242241606L;
@@ -26,17 +38,4 @@ public class Customer implements Serializable {
 
 	@Column(name = "lastname")
 	private String lastName;
-
-	protected Customer() {
-	}
-
-	public Customer(String firstName, String lastName) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
-	}
 }
